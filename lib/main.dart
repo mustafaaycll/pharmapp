103b8f86e60ed2e6993dbbe97da382673fbac79c
Tuğberk ABDULKADİROĞLU <tugberkabdulkadiroglu@Tugberk-MacBook-Pro.local>
2021-11-16 22:19:54 +0300

    Default Base

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:pharm_app/screens/authentication/login.dart';
+import 'package:pharm_app/screens/authentication/sign_up.dart';
+import 'package:pharm_app/screens/basket/basket.dart';
+import 'package:pharm_app/screens/categories/categories.dart';
+import 'package:pharm_app/screens/home.dart';
+import 'package:pharm_app/screens/profile/profile.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+      routes: {
+        '/home': (context) => Home(),
+        '/profile': (context) => Profile(),
+        '/categories': (context) => Categories(),
+        '/basket': (context) => Basket(),
+        '/login': (context) => Login(),
+        '/signup': (context) => SignUp(),
+      },
+    ),
+  );
+}
