df552b479f9eba224c17fa2a494b122ab4c5bc65
mfkmrcler <94452557+mfkmrcler@users.noreply.github.com>
2021-11-23 19:54:41 +0300

    Update categories.dart

--- lib/screens/categories/categories.dart
+++ lib/screens/categories/categories.dart
@@ -1,10 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:pharm_app/utils/colors.dart';
 
-class Categories extends StatelessWidget {
-  const Categories({Key? key}) : super(key: key);
+class categories extends StatefulWidget {
+  const categories({Key? key}) : super(key: key);
 
   @override
+  _categoriesState createState() => _categoriesState();
+}
+
+class _categoriesState extends State<categories> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        title: Text(
+          'Categories',
+          style: TextStyle(
+              color: AppColors.titleText,
+              fontSize: 26,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.primary,
+        actions: [IconButton(
+          onPressed: buttonPressed,
+          //onPressed: (){
+            //Navigator.pushNamed(context, '/filters');
+          //},
+          icon: Icon(
+            Icons.settings, // filtreleme iconu gelecek.
+            color: Colors.white,
+          ),
+        ),
+        ],
+      ),
+      body: PageView(
+        children: [
+          SingleChildScrollView(
+            child: Center(
+              child: Column(
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children:[
+                  SizedBox(height: 16,),
+                  OutlinedButton(
+                    onPressed: buttonPressed,//onpressed is gonna be changed
+                    child: Text(
+                      'Painkillers',
+                      style: TextStyle(
+                          color: AppColors.bodyText,
+                          fontSize: 26,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.secondary50percent,
+                      minimumSize: Size(375.0,100.0),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  OutlinedButton(
+                    onPressed: buttonPressed,//onpressed is gonna be changed
+                    child: Text(
+                      'Dental Care',
+                      style: TextStyle(
+                          color: AppColors.bodyText,
+                          fontSize: 26,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.secondary50percent,
+                      minimumSize: Size(375.0,100.0),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  OutlinedButton(
+                    onPressed: buttonPressed,//onpressed is gonna be changed
+                    child: Text(
+                      'Cosmetics',
+                      style: TextStyle(
+                          color: AppColors.bodyText,
+                          fontSize: 26,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.secondary50percent,
+                      minimumSize: Size(375.0,100.0),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  OutlinedButton(
+                    onPressed: buttonPressed,//onpressed is gonna be changed
+                    child: Text(
+                      'Vitamins',
+                      style: TextStyle(
+                          color: AppColors.bodyText,
+                          fontSize: 26,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.secondary50percent,
+                      minimumSize: Size(375.0,100.0),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  OutlinedButton(
+                    onPressed: buttonPressed,//onpressed is gonna be changed
+                    child: Text(
+                      'Non-Prescription Drugs',
+                      style: TextStyle(
+                          color: AppColors.bodyText,
+                          fontSize: 26,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.secondary50percent,
+                      minimumSize: Size(375.0,100.0),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  OutlinedButton(
+                    onPressed: buttonPressed,//onpressed is gonna be changed
+                    child: Text(
+                      'Category6',
+                      style: TextStyle(
+                          color: AppColors.bodyText,
+                          fontSize: 26,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.secondary50percent,
+                      minimumSize: Size(375.0,100.0),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  OutlinedButton(
+                    onPressed: buttonPressed, //onpressed is gonna be changed
+                    child: Text(
+                      'Category7',
+                      style: TextStyle(
+                          color: AppColors.bodyText,
+                          fontSize: 26,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.secondary50percent,
+                      minimumSize: Size(375.0,100.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+// wont be used.
+void buttonPressed(){
+  print('Button tapped');
+}
+
+
