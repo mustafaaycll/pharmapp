77d961576e9aa00b0b2f09962d474cbed1fede42
Dbatu00 <denizbatu@sabanciuniv.edu>
2021-11-28 20:56:59 +0300

    Basket dart tamam sayılır
    renklendirme lazım

--- lib/screens/basket/basket.dart
+++ lib/screens/basket/basket.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:pharm_app/utils/colors.dart';
+import 'package:pharm_app/utils/dimensions.dart';
 
 
 class Basket extends StatefulWidget {
@@ -13,18 +15,178 @@ class _BasketState extends State<Basket> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: Text('Basket',
+        title: Text(
+          'Basket',
           style: TextStyle(
-              color: AppColors.titleText,
-              fontSize: 26
+            color: AppColors.titleText,
+            fontSize: 26,
           ),
         ),
-        backgroundColor: AppColors.primary,
         centerTitle: true,
+        backgroundColor: AppColors.primary,
         elevation: 0.0,
+
+        ),
+
+      body: PageView(
+        children: [
+          SingleChildScrollView(
+            child: Center(
+              child:
+                Column(
+                  //crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children:[
+                    SizedBox(height: 8,),
+
+                    ListTile(
+                        title: Text('itemInBasket'),
+                        subtitle:Text("Price: XYZ\$"),
+                        trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {},
+                      ),
+                      ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('itemInBasket'),
+                      subtitle:Text("Price: XYZ\$"),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                    
+                    SizedBox(height: 20),
+
+                    Align(
+                      alignment: FractionalOffset.bottomCenter,
+                        child:
+                        Row(
+                            children:[
+                              Align(
+                                alignment: FractionalOffset.bottomLeft,
+                                child: Text(
+
+
+                                  '   Total Price: XXX\$',
+                                  style: TextStyle(
+                                    color: AppColors.bodyText,
+                                    fontSize: 23,
+                                  ),
+
+
+                                ),
+
+                              ),
+                              SizedBox(width: 20),
+                              Align(
+
+                                alignment: FractionalOffset.bottomRight,
+                                child: OutlinedButton(
+                                  child:
+                                  Text(
+                                    'Check Out',
+                                    style: TextStyle(
+                                      color: AppColors.bodyText,
+                                      fontSize: 26,
+                                    ),
+                                  ),
+                                  onPressed: () {},
+
+                                ),
+                              ),
+
+
+                            ]
+
+                        ),
+
+                    )
+
+
+
+                ],
+                ),
+           ),
+          ),
+
+
+
+        ],
       ),
-      body: Center(child: Text('Basket Here')),
     );
   }
 }
